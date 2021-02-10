ce218b5a4d7c50bfaa39a9e0df5ce6c0c927f07a
KevinWang <Kevinvin1997@gmail.com>
2021-02-11 02:00:19 +0800

    完成第一章

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_journey/main.dart';
+import 'file:///C:/AndroidDemo/flutter_journey/lib/day01/01/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
